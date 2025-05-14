8e0b3633666ecc2f7d4e54b1dacc9006c5d25027
Vinod Kumar Kayartaya <vinod@vinod.co>
2025-05-14 17:00:20 +0530

    updated api url

--- src/App.js
+++ src/App.js
@@ -1,17 +1,10 @@
 import { useEffect, useState } from 'react';
 
-console.log('Environment Variables:', process.env);
-const PRODUCTS_API_HOST =
-  process.env.REACT_APP_PRODUCTS_API_HOST || 'localhost';
-const PRODUCTS_API_PORT = process.env.REACT_APP_PRODUCTS_API_PORT || '8000';
-
-const url = `http://${PRODUCTS_API_HOST}:${PRODUCTS_API_PORT}/api/products`;
-
 function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch(url)
+    fetch(process.env.REACT_APP_API_URL || 'http://localhost:8000')
       .then((response) => response.json())
       .then((data) => setProducts(data))
       .catch((error) => console.error('Error fetching products:', error));
